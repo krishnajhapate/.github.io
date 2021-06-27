c862dfb453028d6c0932d95bfd9add1dff1723b4
Krishna Jhapate <ki54.jhapate@gmail.com>
2021-06-27 12:11:46 +0530

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,105 @@
+// Firebase
+
+// Animation
+function loaderPageAni() {
+  let loader = document.querySelector(".loader__content");
+  let num = 6;
+  let discColors = [
+    "#FF0018",
+    "#FF982C",
+    "#FFFF41",
+    "#00D088",
+    "#0088F9",
+    "#D46FC4",
+  ];
+  for (i = 0; i < num; i++) {
+    let disc = document.createElement("div");
+    disc.setAttribute("class", "disc");
+    Object.assign(disc.style, {
+      height: `${i * 10 + 150}px`,
+      width: `${i * 10 + 150}px`,
+      borderTopWidth: "5px",
+      borderTopColor: discColors[5 - i],
+      animationDelay: `${i / 10}s`,
+    });
+    loader.appendChild(disc);
+  }
+}
+
+loaderPageAni();
+
+window.addEventListener("load", () => {
+  document.querySelector(".loader").remove();
+  document.querySelector(".body__content").style.display = "block";
+});
+
+// contact form post
+async function submitForm(event) {
+  event.preventDefault();
+  const url = "http://localhost:8000/api/contact";
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const project = document.getElementById("project");
+  const message = document.getElementById("message");
+  console.log(name.value, email.value, project.value, message.value);
+  data = {
+    name: "name",
+    email: "email@email.com",
+    project: "project",
+    message: "message",
+  };
+
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "no-cors", // no-cors, *cors, same-origin
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+      // "Access-Control-Allow-Origin": "http://127.0.0.1:5500/index.html",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      name: "name",
+      email: "email@email.com",
+      project: "project",
+      message: "message",
+    }), // body data type must match "Content-Type" header
+  })
+    .then((res) => {
+      console.log(res)
+      console.log(res.status)})
+    .catch(function (err) {
+      console.log("Fetch Error :-S", err);
+    });
+  // console.log(response);
+  // console.log(response.json());
+
+  // let messageDisplay = {
+  //   error: "Message not sent! Something went wrong..",
+  //   success: "Message sent ! Thank you for contacting me.",
+  // };
+  // let error;
+  // let msgDiv = document.getElementById("msg");
+  // console.log(msgDiv);
+  // let div = document.createElement("div");
+  // let span = document.createElement('span')
+  // // div.appendChild(span)
+  // document.getElementById('msg').appendChild(div);
+  // console.log(document.getElementsByClassName('message'))
+  // div.class.add('message')
+}
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
+
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
@@ -32,7 +128,6 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent = document.getElementsByClassName("skills__content");
 const skillsHeader = document.querySelectorAll(".skills__header");
-console.log(skillsHeader)
 
 function toggleSkills() {
   let itemClass = this.parentNode.className;
@@ -45,22 +140,168 @@ function toggleSkills() {
   }
 }
 
-skillsHeader.forEach((el)=>{
-    el.addEventListener("click",toggleSkills)
-})
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+// let swiperPortfolio = new Swiper(".portfolio__container", {
+//   cssMode: true,
+//   loop: true,
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
+
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  // cssMode: true,
+  loop: true,
+  grabCursor: true,
+  spaceBetween: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      const activeLink = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+      activeLink?.classList.add("active-link");
+    } else {
+      const activeLink = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+      activeLink?.classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // when the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/
 
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scoll is higher than 560 viewport height, add the scroll class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
 /*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// Previosly selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// We obtain the current thme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.body.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fullfilled, we ask what the issue was to know if we activated
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedTheme === "uil-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  // We save the them and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
